ba1486cd6d1274eeeca44c2af2c4009243d3af84
Unknown_Coder <akshaykumar954012@gmail.com>
2026-02-19 18:22:25 +0530

    Implement file reading from data.txt
    Read a string from 'data.txt' and print it to the console.

--- fileProb/Ques-2/prob.cpp
+++ fileProb/Ques-2/prob.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
 
+int main(){
+// 2. Read from a file
+//     Write a c++ program that creates a file named data.txt and writes
+
+        string str2;
+        ifstream in("data.txt");
+        getline(in,str2);
+        cout << str2;
+        
+        return 0;
+}
